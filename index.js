30fd34ec15078455096f8594124734997ee4b113
Wellington Aires <114850289+wrma80@users.noreply.github.com>
2024-08-21 12:49:53 -0300

    Add passenger count files again

---
+++ index.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    saveEl.textContent += count + " - ";
+    countEl.textContent = 0;
+    count = 0;
+};
